fe06ef4fec8883443e9523288d0520f024787975
vyasishanatc194 <vyasishanatc194@gmail.com>
2021-03-24 19:39:04 +0530

    updated

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -129,6 +129,42 @@ $(".owl-carousel-popular").owlCarousel({
     }
 });
 
+$(".owl-carousel-best").owlCarousel({
+    loop:true,
+    margin:27,
+    smartSpeed:2000,
+    autoplay:true,
+    autoplayTimeout:4000,
+    autoplayHoverPause:true,
+    dots: false,
+    responsiveClass:true,
+    navText: [
+        "<i class='fa fa-chevron-left'></i>",
+        "<i class='fa fa-chevron-right'></i>",
+    ],
+    responsive:{
+        0:{
+            items:3,
+            nav:true
+        },
+        768:{
+            items:4,
+            nav:false
+        },
+
+        1000:{
+            items:5,
+            nav:true
+        },
+        1024:{
+            items:5,
+            nav:true,
+            loop:true
+        },
+       
+    }
+});
+
 $(".nav-toggle").click(function(){
     $(".nav-toggle").removeClass("active");
     $(".nav-toggle").parent().removeClass("active");
